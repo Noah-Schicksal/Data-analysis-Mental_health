50caccbf0b0e8784f1dbca0b59f5c5cdda70f039
Michael Jhonathan <noahsch10833@gmail.com>
2026-03-02 19:01:00 -0300

    standardization in age

--- scripts/remover_outliers_idade.py
+++ scripts/remover_outliers_idade.py
@@ -12,14 +12,9 @@ idade_min = validos.min()
 idade_max = validos.max()
 mediana = validos.median()
 
-
 df.loc[
         (col < idade_min) | (col > idade_max),
         "Age"
     ] = mediana
 
 df.to_csv(caminho, index=False, encoding="utf-8")
-
-
-
-    
